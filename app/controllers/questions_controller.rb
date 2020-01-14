bbf56ef699a3a9e7b92366e47a77929d91cd9c58
Jamie Reynolds <jamie.s.a.reynolds@gmail.com>
2020-01-14 21:17:00 +0000

    Progress so far

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @ask = params[:question]
+
+    if @ask == 'I am going to work'
+      @coach = 'Great!'
+    elsif @ask.to_s.include? '?'
+      @coach = 'Silly question, get dressed and go to work!'
+    else
+      @coach = 'I don\'t care, get dressed and go to work!'
+    end
+  end
+end
